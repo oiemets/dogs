93ea38f79350dbd23c349002548586eb63246da0
olek <iemets.git@gmail.com>
2021-04-16 19:43:30 +0300

    added IconFrame component

--- src/components/IconFrame/IconFrame.tsx
+++ src/components/IconFrame/IconFrame.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import styles from './IconFrame.module.css';
 import classNames from 'classnames/bind';
+import { ButtonLabel } from '../ButtonLabel/ButtonLabel';
+import { Button } from '../Button/Button';
+import { ImagesSearch, VoteTable, PetBreeds } from '../../assets';
 
 type IconFrameProps = {
-  icon?: JSX.Element;
+  icon: keyof typeof iconsMap;
+  title?: string;
+  onClick?: (e: React.SyntheticEvent) => void;
 }
 
 const styleClasses = classNames.bind(styles);
 
-export const IconFrame: React.FC<IconFrameProps> = ({ children }) => {
-  console.log(children);
-  
-  return (
-    <span className={styleClasses('frame')}>
+const iconsMap = {
+  imagesSearch: ImagesSearch,
+  voteTable: VoteTable,
+  petBreeds: PetBreeds
+};
 
+export const IconFrame: React.FC<IconFrameProps> = ({ 
+    onClick, 
+    title, 
+    icon 
+  }) => {
+  
+  const Icon = iconsMap[icon];
+
+  return (
+    <span className={styleClasses('frame')} onClick={onClick}>
+
+      <ButtonLabel 
+        className={styleClasses('icon')} 
+        variant='purpleBlue'>
+        <Icon/>
+      </ButtonLabel>
+
+      <Button className={styleClasses('btn')}>{title}</Button>
+      
     </span>
   );
-}
+};
--- src/components/_testing/BtnIcons.tsx
+++ src/components/_testing/BtnIcons.tsx
@@ -1,44 +1,35 @@
 import React from 'react';
 import { Button } from '../Button/Button';
 import { IconButton } from '../IconButton/IconButton';
-import { ButtonLabel } from '../ButtonLabel/ButtonLabel';
 import { IconFrame } from '../IconFrame/IconFrame';
-import { ImagesSearch, PetBreeds, VoteTable } from '../../assets';
-// import styles from './test.module.css';
 
-// const testBtnClick = (e: React.SyntheticEvent): void => {
-//   console.log('clicked');
-// };
+const testBtnClick = (e: React.SyntheticEvent): void => {
+  console.log('clicked');
+};
 
 export const BtnIcons: React.FC = () => {
   return (
-    <div style={{backgroundColor: '#C7C7C7', padding: '20px'}}>
+    <div style={{
+      backgroundColor: '#C7C7C7', 
+      padding: '20px',
+      width: '1000px',
+      margin: '0 auto',
+      display: 'flex',
+      flexFlow: 'row wrap',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
 
-      
+    <IconFrame title='voting' icon='voteTable' onClick={testBtnClick}/>
+    <IconFrame title='breeds' icon='petBreeds' onClick={testBtnClick}/>
+    <IconFrame title='gallery' icon='imagesSearch' onClick={testBtnClick}/>
 
-      
-      <Button>voting</Button>
-      <Button variant='satin'>upload photo</Button>
+    <Button>button</Button>
+    <IconButton icon='heartFilled'/>
+    <IconButton icon='heart' />
+    <IconButton icon='orderUp' variant='gray'/>
+    <IconButton icon='heart' variant='satin'/>
 
-      <IconButton icon='heart'/>
-      <IconButton icon='arrowLeft'/>
-      <IconButton icon='error'/>
-      <IconButton icon='heartFilled'/>
-
-      <IconButton icon='orderDown' variant='gray'/>
-      <IconButton icon='orderUp' variant='gray'/>
-
-      <ButtonLabel variant='purpleBlue'>
-        <PetBreeds/>
-      </ButtonLabel>
-
-      <ButtonLabel variant='purpleBlue'>
-        <VoteTable/>
-      </ButtonLabel>
-
-      <ButtonLabel variant='purpleBlue'>
-        <ImagesSearch/>
-      </ButtonLabel>
       
 
     </div>
